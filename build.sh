343c28cfb53890c59396c8df98e87b1308112503
Hoang Anh <43473412+thatchloe@users.noreply.github.com>
2025-06-18 18:01:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y build-essential python3-dev
+
+
+# Upgrade essential tools
+pip install --upgrade pip setuptools wheel
+
+# Install Python dependencies
+pip install -r requirements.txt
+
