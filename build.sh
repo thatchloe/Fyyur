ba9f14eb6e8fb784dd249568418818f00f3baf35
Hoang Anh <43473412+thatchloe@users.noreply.github.com>
2025-06-18 18:12:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 
 # Upgrade essential tools
 pip install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel
+pip install --upgrade greenlet
 
 # Install Python dependencies
 pip install -r requirements.txt
