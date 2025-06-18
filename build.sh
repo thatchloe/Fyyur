32bf1a240c5a93c2991384baee147cb70e905bab
Hoang Anh <43473412+thatchloe@users.noreply.github.com>
2025-06-18 18:28:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 
 # Upgrade essential tools
 pip install --upgrade pip setuptools wheel
-pip install --upgrade pip setuptools wheel
-pip install --upgrade greenlet
 
+pip install --only-binary :all: greenlet
+pip install --only-binary :all: Flask-SQLAlchemy
 # Install Python dependencies
 pip install -r requirements.txt
 
